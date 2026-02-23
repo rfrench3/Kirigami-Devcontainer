e1e2f24356e25f94fcfaa7416196d88b00441efc
Robert French <178658682+rfrench3@users.noreply.github.com>
2026-02-23 18:22:26 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+/*
+    SPDX-License-Identifier: GPL-2.0-or-later
+    SPDX-FileCopyrightText: %{CURRENT_YEAR} %{AUTHOR} <%{EMAIL}>
+*/
+
+#include <QtGlobal>
+
+#include <QApplication>
+
+
+#include <QIcon>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickStyle>
+#include <QUrl>
+
+#include "app.h"
+#include "version-kirigamitemplate.h"
+#include <KAboutData>
+#include <KLocalizedContext>
+#include <KLocalizedString>
+
+#include "kirigamitemplateconfig.h"
+
+using namespace Qt::Literals::StringLiterals;
+
+int main(int argc, char *argv[])
+{
+
+    QApplication app(argc, argv);
+
+    // Default to org.kde.desktop style unless the user forces another style
+    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+        QQuickStyle::setStyle(u"org.kde.desktop"_s);
+    }
+
+    KLocalizedString::setApplicationDomain("kirigamitemplate");
+    QCoreApplication::setOrganizationName(u"KDE"_s);
+
+    KAboutData aboutData(
+        // The program name used internally.
+        u"kirigamitemplate"_s,
+        // A displayable program name string.
+        i18nc("@title", "KirigamiTemplate"),
+        // The program version string.
+        QStringLiteral(KIRIGAMITEMPLATE_VERSION_STRING),
+        // Short description of what the app does.
+        i18n("Application Description"),
+        // The license this code is released under.
+        KAboutLicense::GPL,
+        // Copyright Statement.
+        i18n("(c) %{CURRENT_YEAR}"));
+    aboutData.addAuthor(i18nc("@info:credit", "%{AUTHOR}"),
+                        i18nc("@info:credit", "Maintainer"),
+                        u"%{EMAIL}"_s,
+                        u"https://yourwebsite.com"_s);
+    aboutData.setTranslator(i18nc("NAME OF TRANSLATORS", "Your names"), i18nc("EMAIL OF TRANSLATORS", "Your emails"));
+    KAboutData::setApplicationData(aboutData);
+    QGuiApplication::setWindowIcon(QIcon::fromTheme(u"org.kde.kirigamitemplate"_s));
+
+    QQmlApplicationEngine engine;
+
+    auto config = KirigamiTemplateConfig::self();
+
+    qmlRegisterSingletonInstance("org.kde.kirigamitemplate.private", 1, 0, "Config", config);
+
+    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+    engine.loadFromModule("org.kde.kirigamitemplate", u"Main");
+
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
+}
